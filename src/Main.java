b01605cb6cbd03260d23b3b5952ba1d4f1448be3
arasopraza <aras.opraza@gmail.com>
2020-10-17 17:35:20 +0700

    Initial Commit :tada:

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * @author
+ * NAMA     : Arsy Opraza Akma
+ * KELAS    : IF1
+ * NIM      : 10119001
+ * Deskripsi Program: Program ini berisi operator logika
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b " + (a && b));
+        System.out.println("a || b " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+
+    }
+}
